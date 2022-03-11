18b971dd49250a23069ff425628905f91bf802ad
HenryFerreira <henryferreira133@gmail.com>
2022-03-11 14:13:40 -0300

    Se modifico el ValueObject Direccion

--- src/main/java/co/com/sofka/retofinal/genericos/direccion/Direccion.java
+++ src/main/java/co/com/sofka/retofinal/genericos/direccion/Direccion.java
@@ -6,12 +6,27 @@ import java.util.Objects;
 
 public class Direccion implements ValueObject<String> {
     private final String value;
+    private final Calle calle;
+    private final Ciudad ciudad;
+    private final NroPuerta nroPuerta;
 
-    public Direccion(String value) {
-        this.value = Objects.requireNonNull(value);
-        if (this.value.isBlank()) {
-            throw new IllegalArgumentException("La direccion no puede estar vacia");
+    public Direccion(Calle calle, Ciudad ciudad, NroPuerta nroPuerta) {
+        this.calle = Objects.requireNonNull(calle);
+        this.ciudad = Objects.requireNonNull(ciudad);
+        this.nroPuerta = Objects.requireNonNull(nroPuerta);
+        if (this.calle.value().isBlank()) {
+            throw new IllegalArgumentException("La calle no puede estar vacia");
         }
+        if (this.ciudad.value().isBlank()) {
+            throw new IllegalArgumentException("La ciudad no puede estar vacia");
+        }
+        if (this.nroPuerta.value().isBlank()) {
+            throw new IllegalArgumentException("El numero de puerta no puede estar vacio");
+        }
+
+        this.value = "Ciudad: " + this.ciudad + "\n" +
+                "Calle: " + this.ciudad + "\n" +
+                "Numero de puerta: " + this.nroPuerta;
     }
 
     public String value() {
