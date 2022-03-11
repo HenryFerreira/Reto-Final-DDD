e6979173f1e078a94cb813f967901eecc1b55df3
HenryFerreira <henryferreira133@gmail.com>
2022-03-10 22:44:54 -0300

    Se implementaron los metodos del [AR] EncargadoVenta y Vehiculo. Tambien se modifico el Modelo de dominio

--- src/main/java/co/com/sofka/retofinal/encargadoventa/EncargadoVenta.java
+++ src/main/java/co/com/sofka/retofinal/encargadoventa/EncargadoVenta.java
@@ -1,11 +1,13 @@
 package co.com.sofka.retofinal.encargadoventa;
 
 import co.com.sofka.domain.generic.AggregateEvent;
+import co.com.sofka.retofinal.cliente.Compra;
 import co.com.sofka.retofinal.encargadoventa.events.EncargadoVentaCreado;
 import co.com.sofka.retofinal.encargadoventa.values.EncargadoVentaID;
 import co.com.sofka.retofinal.genericos.Nombre;
 
 import java.util.List;
+import java.util.Objects;
 
 public class EncargadoVenta extends AggregateEvent<EncargadoVentaID> {
     protected List<Venta> ventas;
@@ -17,4 +19,32 @@ public class EncargadoVenta extends AggregateEvent<EncargadoVentaID> {
         super(encargadoVentaID);
         appendChange(new EncargadoVentaCreado(nombre)).apply();
     }
+
+    public void listarVentas() {
+        System.out.println("Lista de Ventas: \n");
+        for (Venta elemento : this.ventas) {
+            System.out.println(elemento.detallesVenta());
+        }
+    }
+
+    public void listarMetas() {
+        System.out.println("Lista de Metas: \n");
+        for (Meta elemento : this.metas) {
+            System.out.println("Objetivo: " + elemento.objetivo().value());
+            System.out.println("Beneficio: " + elemento.bonificacion().value());
+        }
+    }
+
+    public void agregarVenta(Venta venta) {
+        this.ventas.add(Objects.requireNonNull(venta));
+    }
+
+    public void agregarMeta(Meta meta) {
+        this.metas.add(Objects.requireNonNull(meta));
+    }
+
+    public void actualizarNombre(Nombre nombre) {
+        this.nombre = Objects.requireNonNull(nombre);
+    }
+
 }
--- src/main/java/co/com/sofka/retofinal/vehiculo/Vehiculo.java
+++ src/main/java/co/com/sofka/retofinal/vehiculo/Vehiculo.java
@@ -4,13 +4,31 @@ import co.com.sofka.domain.generic.AggregateEvent;
 import co.com.sofka.retofinal.vehiculo.events.VehiculoCreado;
 import co.com.sofka.retofinal.vehiculo.values.VehiculoID;
 
+import java.util.Objects;
+
 public class Vehiculo extends AggregateEvent<VehiculoID> {
     protected FichaTecnica fichaTecnica;
     protected Matriculacion matriculacion;
     protected Garantia garantia;
 
-    public Vehiculo(VehiculoID entityId) {
+    public Vehiculo(VehiculoID entityId, Matriculacion matriculacion) {
         super(entityId);
         appendChange(new VehiculoCreado()).apply();
+        this.matriculacion = matriculacion;
+    }
+
+    public void detallesMatriculacion() {
+        System.out.println(this.matriculacion.detallesMatriculacion());
+    }
+
+    public void detallesFichaTecnica() {
+        System.out.println("Detalles de la Ficha Tecnica: \n");
+        System.out.println(this.fichaTecnica.identity().value());
+        System.out.println(this.fichaTecnica.nombre().value());
+        System.out.println(this.fichaTecnica.descripcion().value());
+    }
+
+    public void asignarGarantia(Garantia garantia) {
+        this.garantia = Objects.requireNonNull(garantia);
     }
 }
