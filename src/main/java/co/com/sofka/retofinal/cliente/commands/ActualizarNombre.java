50e56989d14d9ff000687ca9fdb04f1d2074134a
HenryFerreira <henryferreira133@gmail.com>
2022-03-11 20:49:11 -0300

    Se creo el Caso de Uso ActualizarNombreUseCase y se hizo un Test con el mismo. Tambien se renombro el Comando AcutalizarNombre a ActualizarNombre

--- src/main/java/co/com/sofka/retofinal/cliente/commands/ActualizarNombre.java
+++ src/main/java/co/com/sofka/retofinal/cliente/commands/ActualizarNombre.java
@@ -4,11 +4,11 @@ import co.com.sofka.domain.generic.Command;
 import co.com.sofka.retofinal.cliente.values.ClienteID;
 import co.com.sofka.retofinal.genericos.Nombre;
 
-public class AcutalizarNombre extends Command {
+public class ActualizarNombre extends Command {
     private final ClienteID clienteID;
     private final Nombre nombre;
 
-    public AcutalizarNombre(ClienteID clienteID, Nombre nombre) {
+    public ActualizarNombre(ClienteID clienteID, Nombre nombre) {
         this.clienteID = clienteID;
         this.nombre = nombre;
     }
--- src/main/java/co/com/sofka/usecases/ActualizarNombreUseCase.java
+++ src/main/java/co/com/sofka/usecases/ActualizarNombreUseCase.java
@@ -4,20 +4,16 @@ import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
 import co.com.sofka.retofinal.cliente.Cliente;
-import co.com.sofka.retofinal.cliente.commands.AgregarCompra;
+import co.com.sofka.retofinal.cliente.commands.ActualizarNombre;
 
-public class AgregarCompraUseCase extends UseCase<RequestCommand<AgregarCompra>, ResponseEvents> {
+public class ActualizarNombreUseCase extends UseCase<RequestCommand<ActualizarNombre>, ResponseEvents> {
     @Override
-    public void executeUseCase(RequestCommand<AgregarCompra> input) {
+    public void executeUseCase(RequestCommand<ActualizarNombre> input) {
         var comando = input.getCommand();
         var cliente = Cliente.form(comando.getClienteID(), retrieveEvents());
 
-        cliente.agregarCompra(
-                comando.getCompraID(),
-                comando.getVehiculoID(),
-                comando.getEncargadoVentaID(),
-                comando.getFechaPago(),
-                comando.getMonto()
+        cliente.actualizarNombre(
+                comando.getNombre()
         );
         emit().onResponse(new ResponseEvents(cliente.getUncommittedChanges()));
     }
